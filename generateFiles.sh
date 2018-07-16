59fda754de186deb696efec452e1797f3f737747
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-16 17:36:16 +1000

    bash: updated script and rebuilt files

--- generateFiles.sh
+++ generateFiles.sh
@@ -4,7 +4,7 @@ UI_FOLDER="ui"
 
 # Add modules
 rm -rf modules
-git clone https://github.com/republicprotocol/darknode-ui-ts.git modules/darknode-ui-ts
+git clone -b develop https://github.com/republicprotocol/darknode-ui-ts.git modules/darknode-ui-ts
 git clone https://github.com/republicprotocol/darknode-cli.git modules/darknode-cli
 
 # Remove the old UI build folder
@@ -13,16 +13,12 @@ rm -rf "./public/${UI_FOLDER}"
 # Build UI
 cd ./modules/darknode-ui-ts
 npm install
-git checkout develop
-git pull
 PUBLIC_URL="/${UI_FOLDER}" npm run build
 mv build "../../public/${UI_FOLDER}"
 cd ../..
 
 # Build CLI
 cd modules/darknode-cli
-git checkout master
-git pull
 bash generate.sh
 mv -v build/* ../../public
 cd ../..
