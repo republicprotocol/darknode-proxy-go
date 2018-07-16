8784c61b18f9730c74bae67078d45dd7d57936c6
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-16 15:52:23 +1000

    bash: removed submodules and tweaked script for heroku deployment

--- generateFiles.sh
+++ generateFiles.sh
@@ -2,11 +2,17 @@
 
 UI_FOLDER="ui"
 
+# Add modules
+rm -rf modules
+git clone https://github.com/republicprotocol/darknode-ui-ts.git modules/darknode-ui-ts
+git clone https://github.com/republicprotocol/darknode-cli.git modules/darknode-cli
+
 # Remove the old UI build folder
 rm -rf "./public/${UI_FOLDER}"
 
 # Build UI
 cd ./modules/darknode-ui-ts
+npm install
 git checkout develop
 git pull
 PUBLIC_URL="/${UI_FOLDER}" npm run build
