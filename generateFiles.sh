a0e22c1603ab09d381e4e2ac7ecfdfa7d715ef86
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-17 10:56:33 +1000

    bash, public: cleaned up script and rebuilt project

--- generateFiles.sh
+++ generateFiles.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+MODULES_FOLDER="modules"
 UI_FOLDER="ui"
 
 # Add modules
-rm -rf modules
-git clone https://github.com/republicprotocol/darknode-ui-ts.git modules/darknode-ui-ts
-git clone https://github.com/republicprotocol/darknode-cli.git modules/darknode-cli
+if [ -d "$MODULES_FOLDER" ]; then
+    cd "$MODULES_FOLDER/darknode-ui-ts"
+    git pull
+    cd ../darknode-cli
+    git pull
+    cd ../..
+else
+    git clone https://github.com/republicprotocol/darknode-ui-ts.git "$MODULES_FOLDER/darknode-ui-ts"
+    git clone https://github.com/republicprotocol/darknode-cli.git "$MODULES_FOLDER/darknode-cli"
+fi
 
 # Remove the old UI build folder
-rm -rf "./public/${UI_FOLDER}"
+rm -rf "public/$UI_FOLDER"
 
 # Build UI
-cd ./modules/darknode-ui-ts
+cd "$MODULES_FOLDER/darknode-ui-ts"
 npm install
-PUBLIC_URL="/${UI_FOLDER}" npm run build
-mv build "../../public/${UI_FOLDER}"
+PUBLIC_URL="/$UI_FOLDER" npm run build
+mv build "../../public/$UI_FOLDER"
 cd ../..
 
 # Build CLI
-cd modules/darknode-cli
+cd "$MODULES_FOLDER/darknode-cli"
 bash generate.sh
 mv -v build/* ../../public
 cd ../..
