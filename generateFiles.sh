6fbbd3ce28de176ba7569c412a29aca99cf22d1a
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-20 11:50:45 +1000

    bash: cleaned up script and forcing master branch when building files

--- generateFiles.sh
+++ generateFiles.sh
@@ -4,29 +4,31 @@ MODULES_FOLDER="modules"
 UI_FOLDER="ui"
 
 # Add modules
-if [ -d "$MODULES_FOLDER" ]; then
-    cd "$MODULES_FOLDER/darknode-ui-ts"
+if [ -d $MODULES_FOLDER ]; then
+    cd $MODULES_FOLDER/darknode-ui-ts
+    git checkout master
     git pull
     cd ../darknode-cli
+    git checkout master
     git pull
     cd ../..
 else
-    git clone https://github.com/republicprotocol/darknode-ui-ts.git "$MODULES_FOLDER/darknode-ui-ts"
-    git clone https://github.com/republicprotocol/darknode-cli.git "$MODULES_FOLDER/darknode-cli"
+    git clone https://github.com/republicprotocol/darknode-ui-ts.git $MODULES_FOLDER/darknode-ui-ts
+    git clone https://github.com/republicprotocol/darknode-cli.git $MODULES_FOLDER/darknode-cli
 fi
 
 # Remove the old UI build folder
-rm -rf "public/$UI_FOLDER"
+rm -rf public/$UI_FOLDER
 
 # Build UI
-cd "$MODULES_FOLDER/darknode-ui-ts"
+cd $MODULES_FOLDER/darknode-ui-ts
 npm install
-PUBLIC_URL="/$UI_FOLDER" npm run build
-mv build "../../public/$UI_FOLDER"
+PUBLIC_URL=/$UI_FOLDER npm run build
+mv build ../../public/$UI_FOLDER
 cd ../..
 
 # Build CLI
-cd "$MODULES_FOLDER/darknode-cli"
+cd $MODULES_FOLDER/darknode-cli
 bash scripts/generate.sh
 mv -v build/* ../../../public
 cd ../..
